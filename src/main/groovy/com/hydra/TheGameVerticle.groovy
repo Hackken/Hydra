2bd0fd5ca9da9cffbb00e6429b0313ecc5ba9caa
Pawel Szetela <pawel.szetela@lottoland.com>
2017-12-21 21:22:40 +0100

    some shit ;)

--- src/main/groovy/com/hydra/TheGameVerticle.groovy
+++ src/main/groovy/com/hydra/TheGameVerticle.groovy
@@ -1,6 +1,8 @@
 package com.hydra
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.ext.web.Router
+
 /**
  * @author pawel.szetela
  * @since 21/12/2017
@@ -9,8 +11,30 @@ class TheGameVerticle extends AbstractVerticle{
 
     @Override
     void start() throws Exception {
-        vertx.createHttpServer().requestHandler({ request ->
-            request.response().end("!!! Hail Hydra !!!")
-        }).listen(4321);
+        def server = vertx.createHttpServer()
+        def router = Router.router(vertx)
+
+        router.route('/hydra/launch/').handler({ routingContext ->
+
+            // This handler will be called for every request
+            def response = routingContext.response()
+            response.putHeader("content-type", "text/plain")
+
+            // Write to the response and end it
+            response.end("!!! Hail Hydra !!!")
+        })
+
+        router.route('/hydra/pariplay/launch/').handler({ routingContext ->
+
+            // This handler will be called for every request
+            def response = routingContext.response()
+            response.putHeader("content-type", "text/plain")
+
+
+            // Write to the response and end it
+            response.end("!!!Pariplay Hail Hydra !!!")
+        })
+
+        server.requestHandler(router.&accept).listen(4321)
     }
 }
